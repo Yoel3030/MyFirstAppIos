3a64890ad24b3267b7606dc1a36d8b17e1db40de
Yoel <alexisyoel3030@gmail.com>
2022-09-19 20:32:11 +0200

    Init project

---
+++ MyFirstAppYoel/MyFirstAppYoel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstAppYoel
+//
+//  Created by Alumnos on 19/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hola")
+    }
+
+
+}
+
